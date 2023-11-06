6ad5f5f3dcbc625a752574af71a4763957d4f0ff
Naokichi55 <ibosanganba@gmail.com>
2023-11-06 22:53:53 +0900

    Rails(応用)課題6を回答しました.

--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_19_072950) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_30_075902) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "foods", force: :cascade do |t|
+    t.string "name", null: false, comment: "料理名"
+    t.text "comment", comment: "コメント"
+    t.bigint "user_id", null: false, comment: "ユーザーID"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_foods_on_user_id"
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -23,4 +32,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_19_072950) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "foods", "users"
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,18 @@
 # frozen_string_literal: true
 
-User.create!(email: "test@example.com", password: "password")
-puts "ユーザーの初期データインポートに成功しました。"
+email = "test@example.com"
+password = "password"
+ActiveRecord::Base.connection.execute <<~SQL.squish
+  TRUNCATE TABLE users RESTART IDENTITY CASCADE;
+SQL
+
+user1 = User.create!(email: "satou@example.com", password: "password")
+user2 = User.create!(email: "suzuki@example.com", password: "password")
+user3 = User.create!(email: "tanaka@example.com", password: "password")
+
+user3.foods.create!(name: "ゴーヤチャンプル")
+user2.foods.create!(name: "沖縄そば")
+user1.foods.create!(name: "ゴーヤチャンプル")
+
+User.create!(email:, password:)
+puts "ユーザーの初期投入に成功しました。"
